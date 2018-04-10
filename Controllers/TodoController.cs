cc6d702587bf22de0d3e4497c7754e989cdf4c6f
VitalyaKvas <vitalyakvas@gmail.com>
2018-04-10 22:47:46 +0300

    Small fix.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreVueToDo.Models;
 using CoreVueToDo.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
